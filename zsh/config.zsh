c52d3e3b4e93e4dd98519159431b43ff02f72c73
Brian Shumate <brian@brianshumate.com>
2014-02-11 11:04:40 -0500

    no INORE_EOF

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -26,7 +26,6 @@ setopt EXTENDED_HISTORY # add timestamps to history
 setopt PROMPT_SUBST
 setopt CORRECT
 setopt COMPLETE_IN_WORD
-setopt IGNORE_EOF
 
 setopt APPEND_HISTORY # adds history
 # incrementally add history and share it across sessions
