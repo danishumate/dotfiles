92f1d01e32a9e4d40785c1f8b6e0535e19cc7eae
Brian Shumate <brian@couchbase.com>
2014-02-14 11:02:37 -0500

    weird path stuffs

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,14 @@
-PATH="/usr/local/bin:/usr/local/sbin:$PATH:$HOME/.rbenv/bin:$HOME/.rbenv/shims:$ZSH/bin:/Applications/Postgres93.app/Contents/MacOS/bin:/usr/local/sbin:/usr/local/utilities/bin:/Applications/Xcode.app/Contents/Developer/usr/bin:$HOME/Applications/Couchbase Server.app/Contents/Resources/couchbase-core/bin:$GROOVY_HOME/bin"
-export "PATH"
+CBMACPATH="$HOME/Applications/Couchbase Server.app/Contents/Resources/couchbase-core/bin"
+CBSPATH="$HOME/src/private/cbsupport/bin"
+GROOVYPATH="$HOME/sandbox/groovy-2.2.1/bin"
+LOCALPATH="/usr/local/bin:/usr/local/sbin:/usr/local/sbin:/usr/local/utilities/bin"
+POSTGRESPATH="/Applications/Postgres93.app/Contents/MacOS/bin"
+RBENVAPTH="$HOME/.rbenv/bin:$HOME/.rbenv/shims"
+XCODEPATH="/Applications/Xcode.app/Contents/Developer/usr/bin"
+ZSHBINPATH="$HOME/.dotfiles/bin"
+
+PATH="$LOCALPATH:$PATH:$ZSHBINPATH:$CBSPATH:$RBENVAPTH:$POSTGRESPATH:$XCODEPATH:$CBMACPATH:$GROOVYPATH"
+
+export PATH
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
